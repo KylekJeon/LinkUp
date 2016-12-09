c06616170abe221ef93ed5ab00b63d138672bd06
Kyle Jeon <kylekjeon@gmail.com>
2016-12-09 11:21:29 -0500

    Finish styling Event Pages

--- frontend/components/events/event_page.jsx
+++ frontend/components/events/event_page.jsx
@@ -25,20 +25,39 @@ class EventPage extends React.Component {
   }
 
   render(){
+    let userList;
+    if(this.props.currentEventUsers.length > 0){
+      userList = this.props.currentEventUsers.map ((user) => (
+        <li key={user.id} className="event-page-user-list-items">{user.first_name} {user.last_name}</li>
+      ));
+    }
+
+
     return(
       <section className='event-page-container group'>
-        <button className='event-rsvp-button' onClick={this.addUserToEvent}>RSVP!</button>
-        <h1>Coming Soon: {this.props.currentEvent.name}</h1>
-        <div className='event-page-date-time'>
-          <span className='event-page-date'>Date: {this.props.currentEvent.datetime}</span>
-          <span className='event-page-time'>Time:   {this.props.currentEvent.timeOfDay}</span>
-        </div>
-        <div className='event-page-location'>Location: {this.props.currentEvent.location}</div>
-        <div className='event-page-description'>
-          <span>What's happening:</span>
-          <p>{this.props.currentEvent.description}</p>
-        </div>
-    </section>
+        <section className='event-page-content'>
+          <h1>Coming Soon: {this.props.currentEvent.name}</h1>
+          <div className='event-page-date-time'>
+            <span className='event-page-date'>Date: {this.props.currentEvent.datetime}</span>
+            <span className='event-page-time'>Time:   {this.props.currentEvent.timeOfDay}</span>
+          </div>
+          <div className='event-page-location'>Location: {this.props.currentEvent.location}</div>
+          <div className='event-page-description'>
+            <span>What's happening:</span>
+            <p>{this.props.currentEvent.description}</p>
+          </div>
+        </section>
+        <section className='event-page-users group'>
+          <div className='event-page-rsvp'>
+            <span className='event-rsvp-question'>Interested?</span>
+            <button className='event-rsvp-button' onClick={this.addUserToEvent}>RSVP!</button>
+          </div>
+          <ul className='event-page-user-list'>
+            Attending:
+            {userList}
+          </ul>
+        </section>
+      </section>
     );
   }
 }
