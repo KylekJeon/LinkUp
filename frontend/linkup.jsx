ccc06e83e398e734cf6bd833b9d4eef0d7b25fab
Kyle Jeon <kylekjeon@gmail.com>
2017-01-08 13:42:02 -0500

    Move images to AWS

--- frontend/linkup.jsx
+++ frontend/linkup.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { login } from './actions/session_actions';
-
-window.login = login;
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const root = document.getElementById('root');
